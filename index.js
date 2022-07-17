26671ef8d9bf0f2f71629299f539b951e43f4a72
HaradaYasuo <academic.dev0531@gmail.com>
2022-07-17 12:07:51 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchPage = void 0;
-const chalk_1 = require("chalk");
+const chalk = require("chalk");
 const fs = require("fs");
 const path_1 = require("path");
 const dataLogger = () => {
     console.log("suepr data");
-    console.log(chalk_1.default.green("asdasd"));
+    console.log(chalk.green("asdasd"));
     console.log(fs.existsSync(path_1.default.join(process.cwd(), ".shopify-cms")));
 };
 dataLogger();
