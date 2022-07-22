20c5f50105bd0825d92ad2b3f1a5b0711f68666b
HaradaYasuo <academic.dev0531@gmail.com>
2022-07-22 16:25:18 +0200

    Updated theme file upload & init theme settings

--- utils/configure-theme-files.ts
+++ utils/configure-theme-files.ts
@@ -4,11 +4,11 @@ export const configureThemeFiles = (content: string, config: Config) => {
   return content
     .replace(
       /\|\|\|GLOBAL_SECTIONS_ABOVE_LAYOUT\|\|\|/g,
-      config.global_header_sections.map((section) => `{% section '${section}'%}`).join("\n  ")
+      config?.global_header_sections?.map((section) => `{% section '${section}'%}`).join("\n  ")
     )
     .replace(
       /\|\|\|GLOBAL_SECTIONS_BELOW_LAYOUT\|\|\|/g,
-      config.global_footer_sections.map((section) => `{% section '${section}'%}`).join("\n  ")
+      config?.global_footer_sections?.map((section) => `{% section '${section}'%}`).join("\n  ")
     )
     .replace(/\|\|\|DEVELOPMENT_URL\|\|\|/g, config.development_server)
     .replace(/\|\|\|DEPLOYMENT_URL\|\|\|/g, config.deployment_server);
