215d6cc6c7088ff2cda8f7d1e86dbb259b78d077
HaradaYasuo <academic.dev0531@gmail.com>
2022-07-26 13:56:02 +0200

    Updated theme file upload & init theme settings

--- @types/shopify.ts
+++ @types/shopify.ts
@@ -217,18 +217,91 @@ export type ShopifySettingsInput =
   | ShopifyUrl
   | ShopifyVideo_url;
 
+type ExtractSettings<T extends ShopifySection | ShopifySectionBlock> = Extract<
+  T["settings"][number],
+  { id: string; type }
+>;
+
+type ExtractSetting<T extends ShopifySection | ShopifySectionBlock, ID extends string> = Extract<
+  ExtractSettings<T>,
+  { id: ID }
+>;
+
+type MapSettings<Section extends ShopifySection | ShopifySectionBlock> = {
+  [ID in ExtractSettings<Section>["id"]]: ExtractSetting<Section, ID>["type"] extends "article"
+    ? _Article_liquid
+    : ExtractSetting<Section, ID>["type"] extends "checkbox"
+    ? boolean
+    : ExtractSetting<Section, ID>["type"] extends "number" | "range"
+    ? number
+    : ExtractSetting<Section, ID>["type"] extends "radio" | "select"
+    ? // @ts-ignore
+      ExtractSetting<Section, ID>["options"][number]["value"]
+    : ExtractSetting<Section, ID>["type"] extends
+        | "text"
+        | "textarea"
+        | "color_background"
+        | "html"
+        | "liquid"
+        | "url"
+        | "font"
+    ? string
+    : ExtractSetting<Section, ID>["type"] extends "blog"
+    ? _Blog_liquid
+    : ExtractSetting<Section, ID>["type"] extends "collection"
+    ? _Collection_liquid
+    : ExtractSetting<Section, ID>["type"] extends "collection_list"
+    ? _Collection_liquid[]
+    : ExtractSetting<Section, ID>["type"] extends "color"
+    ? _Color_liquid
+    : ExtractSetting<Section, ID>["type"] extends "font_picker"
+    ? _Font_liquid
+    : ExtractSetting<Section, ID>["type"] extends "image_picker"
+    ? _Image_liquid
+    : ExtractSetting<Section, ID>["type"] extends "link_list"
+    ? _Linklist_liquid
+    : ExtractSetting<Section, ID>["type"] extends "page"
+    ? _Page_liquid
+    : ExtractSetting<Section, ID>["type"] extends "product"
+    ? _Product_liquid
+    : ExtractSetting<Section, ID>["type"] extends "product_list"
+    ? _Product_liquid[]
+    : ExtractSetting<Section, ID>["type"] extends "richtext"
+    ? `<p${string}</p>`
+    : ExtractSetting<Section, ID>["type"] extends "video_url"
+    ? ("youtube" | "vimeo")[]
+    : never;
+};
+
+type MapSection<T> = {
+  // @ts-ignore
+  blocks: MapBlocks<T>[keyof MapBlocks<T>][];
+  id: string;
+  // @ts-ignore
+  settings: MapSettings<T>;
+  // @ts-ignore
+  type: Lowercase<T["name"]>;
+};
+
+type MapBlocks<T extends { blocks: ShopifySectionBlock[] }> = {
+  [B in Extract<T["blocks"][number], { type }>["type"]]: {
+    id: string;
+    settings: MapSettings<Extract<T["blocks"][number], { type: B }>>;
+    type: B;
+  };
+};
+
+type MapBlocksPreset<T extends { blocks: ShopifySectionBlock[] }> = {
+  [B in Extract<T["blocks"][number], { type }>["type"]]: {
+    settings: Partial<MapSettings<Extract<T["blocks"][number], { type: B }>>>;
+    type: B;
+  };
+};
+
 type ShopifySectionDefault<T = never> = {
-  blocks?: T extends never
-    ? {
-        settings: { [T: string]: string | number | boolean };
-        type: "step";
-      }[]
-    : T extends { blocks: any }
-    ? Partial<Omit<T["blocks"][number], "id">>[]
-    : {
-        settings: { [T: string]: string | number | boolean };
-        type: "step";
-      }[];
+  blocks?: T extends { blocks: Array<any> }
+    ? MapBlocksPreset<T>[keyof MapBlocksPreset<T>][]
+    : never;
   settings?: T extends never
     ? { [T: string]: string | number | boolean }
     : T extends { settings: any }
@@ -236,19 +309,11 @@ type ShopifySectionDefault<T = never> = {
     : { [T: string]: string | number | boolean };
 };
 
-type ShopifySectionPreset<T = never> = {
+type ShopifySectionPreset<T = unknown> = {
   name: string;
-  blocks?: T extends never
-    ? {
-        settings: { [T: string]: string | number | boolean };
-        type: "step";
-      }[]
-    : T extends { blocks: any }
-    ? Partial<Omit<T["blocks"][number], "id">>[]
-    : {
-        settings: { [T: string]: string | number | boolean };
-        type: "step";
-      }[];
+  blocks?: T extends { blocks: Array<any> }
+    ? MapBlocksPreset<T>[keyof MapBlocksPreset<T>][]
+    : never;
   settings?: T extends never
     ? { [T: string]: string | number | boolean }
     : T extends { settings: any }
